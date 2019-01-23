855e69662da045c560f85b082c8997b3439c90d8
Patrick Paolella <patrickpaolella@gmail.com>
2019-01-23 15:40:32 -0500

    Bump version to 3.0 and have pip automatically install dependencies (#8)
    * Bump version to 3.0 and have pip automatically install dependencies
    
    * Convert double quotes to single quotes

--- setup.py
+++ setup.py
@@ -9,24 +9,28 @@ ver_path = convert_path('log_manager_cli/version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'r') as requirements_file:
+    required = requirements_file.read().splitlines()
+
 setuptools.setup(
-    name="oflog",
+    name='oflog',
     version=main_ns['__version__'],
-    author="OpenFin",
-    author_email="dev@openfin.co",
-    description="CLI for the OpenFin Log Management Service",
+    author='OpenFin',
+    author_email='dev@openfin.co',
+    description='CLI for the OpenFin Log Management Service',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/openfin/log-manager-cli",
+    long_description_content_type='text/markdown',
+    url='https://github.com/openfin/log-manager-cli',
+    install_requires=required,
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: Apache Software License"
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License'
     ),
     entry_points={
-        "console_scripts": ["oflog=log_manager_cli.openfin_log_cli:main"]
+        'console_scripts': ['oflog=log_manager_cli.openfin_log_cli:main']
     }
 )
